d529d301298c1005c6092b8bbacbd9e46c6e47dd
KeHongYing <b06902074@ntu.edu.tw>
2020-09-16 20:10:53 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { IconButton, Box, Slider } from "@material-ui/core";
+import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box className="player">
+        <Slider
+          value={50}
+          // onChange={handleChange}
+          // aria-labelledby="continuous-slider"
+          className="slider"
+        />
+        <div className="Button">
+          <IconButton
+            color="primary"
+            aria-label="upload picture"
+            component="span"
+          >
+            <SkipPreviousIcon fontSize="large" />
+          </IconButton>
+          <IconButton
+            color="primary"
+            aria-label="upload picture"
+            component="span"
+          >
+            <PlayCircleFilledIcon fontSize="large" />
+          </IconButton>
+          <IconButton
+            color="primary"
+            aria-label="upload picture"
+            component="span"
+          >
+            <SkipNextIcon fontSize="large" />
+          </IconButton>
+        </div>
+      </Box>
     </div>
   );
 }
