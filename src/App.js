b209d6c95aa14a69b2e0f2bd11c24cc28eefbce9
KeHongYing <b06902074@ntu.edu.tw>
2020-09-16 21:40:04 +0800

    add state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { IconButton, Box, Slider } from "@material-ui/core";
 import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
 import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
@@ -6,12 +6,22 @@ import SkipNextIcon from "@material-ui/icons/SkipNext";
 import "./App.css";
 
 function App() {
+  const [time, setTime] = useState(0);
+  const handleChange = (_, position) => {
+    setTime(position);
+  };
+  const addOne = () => {
+    setTime((t) => Math.min(t + 1, 100));
+  };
+  const deleteOne = () => {
+    setTime((t) => Math.max(t - 1, 0));
+  };
   return (
     <div className="App">
       <Box className="player">
         <Slider
-          value={50}
-          // onChange={handleChange}
+          value={time}
+          onChange={handleChange}
           // aria-labelledby="continuous-slider"
           className="slider"
         />
@@ -20,6 +30,7 @@ function App() {
             color="primary"
             aria-label="upload picture"
             component="span"
+            onClick={deleteOne}
           >
             <SkipPreviousIcon fontSize="large" />
           </IconButton>
@@ -34,6 +45,7 @@ function App() {
             color="primary"
             aria-label="upload picture"
             component="span"
+            onClick={addOne}
           >
             <SkipNextIcon fontSize="large" />
           </IconButton>
